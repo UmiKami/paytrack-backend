de348493acff2cf1c98295b5a8545b3f3ef9dbaa
UmiKami <ggoku02@gmail.com>
2024-07-14 17:12:46 -0400

    Employee Registration, login, addition, and retrieval

--- src/routes.py
+++ src/routes.py
@@ -3,8 +3,10 @@ from src.models import db, User, Employee, PasswordResetToken
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
+from src.send_email import send_email
 from hashlib import sha256
 import os
+from datetime import datetime, timezone, timedelta
 
 api = Blueprint('api', __name__)
 
@@ -32,7 +34,47 @@ def regisster_admin():
     
     return jsonify(message="Admin user created successfully!")
 
-@api.route("/admin/login", methods=["POST"])
+
+@api.route("/employee/register", methods=["POST"])
+@jwt_required()
+def register_employee():
+    token = PasswordResetToken.query.filter_by(employee_id=get_jwt_identity()).first()
+    
+    if token.expires_at.tzinfo is None:
+        token.expires_at = token.expires_at.replace(tzinfo=timezone.utc)
+    
+    if token is None or token.used or token.expires_at < datetime.now(tz=timezone.utc):
+        return jsonify(message="Invalid or expired token"), 400
+        
+    token.used = True
+
+    db.session.commit()
+    
+    request_body = request.get_json()
+    
+    email = request_body.get("email")
+    password = request_body.get("password")
+    security_question_1 = request_body.get("security_question_1")
+    security_answer_1 = request_body.get("security_answer_1")
+    security_question_2 = request_body.get("security_question_2")
+    security_answer_2 = request_body.get("security_answer_2")
+    
+    if not email or not password or not security_question_1 or not security_answer_1 or not security_question_2 or not security_answer_2:
+        return jsonify(message="All fields are required"), 400
+    
+    password_hash = sha256(password.encode()).hexdigest()
+    
+    user = User(email=email, password_hash=password_hash, security_question_1=security_question_1, security_answer_1=security_answer_1, security_question_2=security_question_2, security_answer_2=security_answer_2, role="employee")
+    db.session.add(user)
+    db.session.commit()
+    db.session.refresh(user)
+    
+    employee = Employee.query.get(get_jwt_identity())
+    employee.user_id = user.user_id
+    
+    return jsonify(message="Employee user created successfully!")
+
+@api.route("/login", methods=["POST"])
 def login_admin():
     request_body = request.get_json()
     
@@ -46,7 +88,65 @@ def login_admin():
     if user is None:
         return jsonify(message="Invalid email or password"), 401
         
-    access_token = create_access_token(identity=user.user_id)
+    access_token = create_access_token(identity=user.user_id, additional_claims={"role": user.role})
     
     return jsonify(access_token=access_token)
+
+@api.route("/manage/employee", methods=["GET"])
+@jwt_required()
+def get_employees():
+    current_user_id = get_jwt_identity()
+    
+    user = User.query.get(current_user_id)
+    
+    if user.role != "admin":
+        return jsonify(message="You are not authorized to perform this action"), 403
+        
+    employees = Employee.query.all()
+    
+    return jsonify([employee.serialize() for employee in employees])
+
+@api.route("/manage/employee/add", methods=["POST"])
+@jwt_required()
+def add_employee():
+    current_user_id = get_jwt_identity()
+    
+    user = User.query.get(current_user_id)
+    
+    if user.role != "admin":
+        return jsonify(message="You are not authorized to perform this action"), 403
+        
+    request_body = request.get_json()
+    
+    email = request_body.get("email")
+    
+    employee = Employee.query.filter_by(personal_email=email).first()
+    
+    if employee is not None:
+        return jsonify(message="Employee with this email already exists"), 400
+    
+    first_name = request_body.get("first_name")
+    last_name = request_body.get("last_name")
+    address = request_body.get("address")
+    phone = request_body.get("phone")
+    position = request_body.get("position")
+    department = request_body.get("department")
+    start_date = request_body.get("start_date")
+    
+    employee = Employee(personal_email=email,first_name=first_name, last_name=last_name, address=address, phone=phone, position=position, department=department, start_date=start_date)
+    db.session.add(employee)
+    db.session.commit()
+    db.session.refresh(employee)
+    
+    password_reset_token = create_access_token(identity=employee.employee_id)
+    
+    token = PasswordResetToken(employee_id=employee.employee_id, token=password_reset_token, expires_at=datetime.now(tz=timezone.utc) + timedelta(hours=24))
+    
+    db.session.add(token)
+    db.session.commit()
+    
+    send_email(url=f"{os.getenv('FRONT_URL')}/employee/password-creation?token={password_reset_token}", name=f"{first_name} {last_name}", recipient=email)
+    
+    
+    return jsonify(message="Employee added successfully!"), 201
 
